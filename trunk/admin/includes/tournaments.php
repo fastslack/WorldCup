1c1b87681c3d5d4bee77b715781d495e7fee460d
Matias Aguirre <maguirre@matware.com.ar>
2014-05-01 04:28:28 -0300

    Update tournament class

--- trunk/admin/includes/tournaments.php
+++ trunk/admin/includes/tournaments.php
@@ -46,7 +46,7 @@ class WorldcupTournaments extends JObject
 	 *
 	 * @return object An object with the groups data.
 	 */
-	public function getGroupsList($tournament)
+	public function getGroupsList($tournament, $flag = 'id')
 	{
 		// Get the correct equipment
 		$query = $this->_db->getQuery(true);
@@ -59,6 +59,71 @@ class WorldcupTournaments extends JObject
 
 		$query->order("g.id ASC");
 		// Retrieve the data.
-		return $this->_db->setQuery($query)->loadObjectList('id');
+		return $this->_db->setQuery($query)->loadObjectList($flag);
+	}
+
+	/**
+	* Get the matches of specific tournament
+	*
+	* @param  int  $tournament  The tournament id.
+	* @param  int  $phase  The phase id.
+	*
+	* @return object An object with the matches data.
+	*/
+	public function getGroupsTable ($tournament, $user_id, $flag = 'bets') {
+
+		// Loader
+		JLoader::import('helpers.worldcup', JPATH_COMPONENT_ADMINISTRATOR);
+
+		$data = array();
+		$db =& JFactory::getDBO();
+
+		$betsObj = new WorldcupBets();
+		$matchesObj = new WorldcupMatches();
+		$teamsObj = new WorldcupTeams();
+
+		// Get the groups
+		$groups = $this->getGroupsList($tournament, null);
+
+		// Each group
+		$i = 1;
+
+		foreach ($groups as $key => $group) {
+
+			// Get teams	
+			$teams[$i] = $teamsObj->getTeamsArray($tournament, $group->id);
+
+			// Get bets
+			if ($flag == 'bets') {
+				$bets = $betsObj->getBetsGroupList($tournament, $user_id, $group->id);
+			}else if ($flag == 'results') {
+				$bets = $matchesObj->getResultsList($tournament, $group->id);
+			}
+
+			// Foreach bets
+			foreach ($bets as $bet)
+			{
+				if ($bet->local > $bet->visit ) {
+					$teams[$i][$bet->team1]['points'] += 3;
+				}else if ($bet->local < $bet->visit ) {
+					$teams[$i][$bet->team2]['points'] += 3;
+				}else if ($bet->local == $bet->visit ) {
+					$teams[$i][$bet->team1]['points'] += 1;
+					$teams[$i][$bet->team2]['points'] += 1;	
+				}
+
+				$teams[$i][$bet->team1]['gf'] += $bet->local;
+				$teams[$i][$bet->team2]['gf'] += $bet->visit;
+
+				$teams[$i][$bet->team1]['ge'] += $bet->visit;
+				$teams[$i][$bet->team2]['ge'] += $bet->local;	
+			}
+
+			$data[$i] = WorldCupHelper::_orderBy($teams[$i]);
+
+			$i++;
+		}
+
+		return $data;
 	}
 }
